d0de9fc74f4e72322dce6fea6875ecd441aaeda0
hacen27 <hacenm144@gmail.com>
2023-01-02 15:18:27 +0000

    compte

--- Rim-banque/src/app/dashboard/dashboard.component.ts
+++ Rim-banque/src/app/dashboard/dashboard.component.ts
@@ -1,36 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
 import { LocalDbService } from '../services/local-db.service';
 import { LoginService } from '../services/login.service';
 import { DashboardService } from '../services/dashboard.service';
-import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-   comptes!: Object
+export class DashboardComponent implements OnInit , AfterViewInit{
+   comptes: Object=[];
 
   constructor(
     private localDbService: LocalDbService,
     private loginService: LoginService,
-    private dashboardService:DashboardService) { 
+    private dashboardService:DashboardService) {
 
   }
+  ngAfterViewInit(): void {
+    this.dashboardService.getComptes().subscribe((datas)=>{
+      this.comptes=datas;
+      console.log(datas)
+      console.log("finsh *****************dashboard!")
+    })  }
 
   ngOnInit(): void {
     console.log("Inisialiser dashboard!")
-    this.dashboardService.getComptes().subscribe((datas)=>{
-      this.comptes=datas;
-    })
+
   }
 
   afficheToken(){
     console.log("la token : "+ this.localDbService.token);
     this.loginService.logOut();
   }
-  
+
   logout(){
     this.loginService.logOut();
   }
--- Rim-banque/src/app/services/dashboard.service.ts
+++ Rim-banque/src/app/services/dashboard.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
   readonly API_URL="http://localhost:9000/comptesmangement";
+  headers = new HttpHeaders({'Access-Control-Allow-Origin' : '*'})
+
   readonly ENDPOINT_COMPTES="/comptes";
+  readonly ENDPOINT_SEARCH_COMPTES="/searchcompte/{codecompte}";
   constructor(private httpClient:HttpClient) { 
 
   }
   getComptes(){
-    return this.httpClient.get(this.API_URL+this.ENDPOINT_COMPTES)
+    return this.httpClient.get(this.API_URL+this.ENDPOINT_COMPTES,{headers: this.headers})
   }
 }
